50c21eef8133fb3e6188e203c505cf772dada536
Adi-K-Coding <34950822+Adi-K-Coding@users.noreply.github.com>
2021-10-24 16:32:39 -0700

    hospital
    fix addpatients

--- src/_04_hospital/Hospital.java
+++ src/_04_hospital/Hospital.java
@@ -1,26 +1,33 @@
 package _04_hospital;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hospital {
+	ArrayList<Doctor> doctors = new ArrayList<Doctor>();
+	ArrayList<Patient> patients = new ArrayList<Patient>();
 
 	public List<Doctor> getDoctors() {
 		// TODO Auto-generated method stub
-		return null;
+		return doctors;
 	}
 
 	public void addDoctor(Doctor type) {
-
+		doctors.add(type);
 	}
 
 	public void addPatient(Patient patient) {
-		// TODO Auto-generated method stub
 		
+
 	}
 
 	public List<Doctor> getPatients() {
-		// TODO Auto-generated method stub
 		return null;
+		
+	}
+
+	public void assignPatientsToDoctors() {
+
 	}
 
 }
--- src/_04_hospital/Patient.java
+++ src/_04_hospital/Patient.java
@@ -2,4 +2,14 @@ package _04_hospital;
 
 public class Patient {
 
+	public Object feelsCaredFor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void checkPulse() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
